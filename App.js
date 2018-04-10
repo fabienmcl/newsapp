b7da6ee6d08ea22c32a2b96f997ca70abcc70a38
fabienmcl <fabien.michl@gmail.com>
2018-04-10 12:31:26 +0200

    Add: Header components

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Header from './components/Header';  
+import Score from './components/Score'; 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>project news app</Text>
+        <Header />
+        <View style={styles.body}>
+          <Text>project news app master</Text>
+        </View>
+        <Score />
       </View>
     );
   }
@@ -18,4 +24,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  body: {
+    flex: 18,
+    justifyContent: 'space-between',
+    padding: 10,
+    marginTop: 20
+  }
 });
