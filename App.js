46c04fdeb1b708f5c6d4a440edca4f6eafa3b111
fabienmcl <fabien.michl@gmail.com>
2018-04-10 15:25:03 +0200

    Add : different list style for App.js

--- App.js
+++ App.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 import Header from './components/Header';  
 import Score from './components/Score'; 
+import ListViewDemo from './components/ListViewDemo1';
+import List from './components/List'; 
+import { establishments }     from './components/fixtures'
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Header />
         <View style={styles.body}>
+        <List />
+        <ListViewDemo /> 
           <Text>project news app master</Text>
+          <FlatList
+            data={[{key: 'a'}, {key: 'b'}]}
+            renderItem={({item}) => <Text>{item.key}</Text>}
+          />
+          <FlatList
+          data = {[
+            { key: 1, name: 'Alice' },
+            { key: 2, name: 'Bob' },
+            { key: 3, name: 'Claire' },
+            { key: 4, name: 'David' },
+            ]}
+            renderItem={({item}) => <Text>{item.key} : {item.name}</Text>}
+          />
+          
         </View>
-        <Score />
       </View>
     );
   }
