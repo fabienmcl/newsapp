381e58ac47391ffb40dc93e8017540a6073460d4
fabienmcl <fabien.michl@gmail.com>
2018-04-12 18:56:37 +0200

    Update : Js injected & onMessage works

--- components/TabNavigator.js
+++ components/TabNavigator.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { TabNavigator } from 'react-navigation';
 import WebV from './WebV';
+import ListV from './ListView'
 
 class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home!</Text>
+        <Text>test mise en place de tarnavigator!</Text>
+        <Text>avec une webView dans</Text>
       </View>
     );
-  }
+  } 
 }
 
 class SettingsScreen extends React.Component {
@@ -21,7 +23,27 @@ class SettingsScreen extends React.Component {
   }
 }
 
+class Recommended extends React.Component {
+    render(){
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>liste des articles en rapport!</Text>
+          </View> 
+        );
+    }
+}
+
 export default TabNavigator({
-  Home: { screen: HomeScreen },
-  Settings: { screen: SettingsScreen },
+  Homme: { screen: HomeScreen },
+  WebView: { screen: SettingsScreen },
+  recommended: { screen: Recommended}
+},
+{
+    swipeEnabled:false,
+    animationEnabled: true,
+    tabBarPosition: 'bottom',
+    tabBarTopComponent: props => {
+    },
 });
+
+//https://reactnavigation.org/docs/tab-based-navigation.html
--- components/WebV.js
+++ components/WebV.js
@@ -5,27 +5,52 @@ import {
     TabNavigator,
   } from 'react-navigation';
 
-export default class WebV extends Component {
-    static navigationOptions = {
-        title: "WebViewScreen"
-    }
-    render() {
-      return (
-        <WebView
-            source={{uri: 'https://www.numerama.com/tech/344470-apple-condamne-a-payer-demi-milliard-de-dollars-violation-de-brevets-a-legalite-douteuse.html'}}
-        /> 
-      )
-    }
-    runJSInBackground (code) {
-      this.webView.injectJavaScript(code)
-    }
-    handleMessage = (e) => {
-      const message = e.nativeEvent.data
-      console.log('message from webview:', message)
-    }
+  const injectedScript = () => {
+    window.postMessage(document.body.innerHTML);
+    window.postMessage = window.originalPostMessage || window.postMessage;
   }
-/*<WebView
-          ref={el => this.webView = el}
-          source={{html: '<html><body></body></html>'}}
-          onMessage={this.handleMessage}
-        />*/
+
+export default class WebV extends Component {
+  constructor(props) {
+    super(props)
+    this.postMessage = this.postMessage.bind(this)
+}
+postMessage(action) {
+    this.WebView.postMessage(JSON.stringify(action))
+}
+  render() {
+    return (
+      <WebView
+          ref={webview => { this.myWebView = webview; }} 
+          source={{uri: 'https://mobile.francetvinfo.fr/internet/reseaux-sociaux/en-inde-un-informaticien-toulousain-decouvre-quune-application-mobile-collecte-des-donnees-sans-autorisation_2684728.html'}}
+          //affiche l'objet au debut et a la fin du telechargement
+          onNavigationStateChange={(navEvent)=> console.log(navEvent)}
+          //javaScriptEnabled = {true}
+          //onLoadEnd = {console.log("webview content : onLoadEnd")}
+          //onLoad = {alert("onLoad")}
+          //scrollEnabled = {true}//false permet de bloquer le scroll
+          
+          //injectedJavaScript={`window.postMessage(JSON.stringify({"score": 1}))`}
+          //onMessage={console.log(JSON.parse(message.nativeEvent.data))} 
+          
+          
+          
+          
+          
+          
+          onMessage={ (event)=>{ const message = event.nativeEvent.data; console.log(message); console.log('message'); console.log(event.nativeEvent); } }
+          //onMessage={() => console.log('Hello World')}
+
+          //injectedJavaScript={'document.write("<h1>Test</h1>")'}
+          javaScriptEnabled = {true}
+
+          injectedJavaScript={" window.postMessage(document.body.innerHTML); window.postMessage = window.originalPostMessage || window.postMessage;setTimeout(function(){ window.postMessage('asdsa', '*'); },1000); "} 
+          //onMessage={e => onMessage(JSON.parse(e.nativeEvent.data))}
+        />
+        
+    ) 
+    //this.webview.postMessage("Hello from RN");
+    this.webview.postMessage('Post message triggered');
+  }
+  
+} 
