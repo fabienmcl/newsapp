c8f8f45ae05a50a854506a290548ccd3729f5b88
fabienmcl <fabien.michl@gmail.com>
2018-04-13 17:54:56 +0200

    Update : message form webView add maxScrollReached

--- components/MessageWebView.js
+++ components/MessageWebView.js
@@ -2,16 +2,38 @@ import React from 'react'
 import { WebView } from 'react-native'
 
 // fix https://github.com/facebook/react-native/issues/10865
+// thx https://github.com/kyle-ilantzis !
 const patchPostMessageJsCode = `(${String(function() {
     var originalPostMessage = window.postMessage
     var patchedPostMessage = function(message, targetOrigin, transfer) {
         originalPostMessage(message, targetOrigin, transfer)
     }
-     
+    window.counter = 0;
+    function msToTime(duration) {
+        var milliseconds = parseInt((duration%1000)/100)
+            , seconds = parseInt((duration/1000)%60)
+            , minutes = parseInt((duration/(1000*60))%60)
+            , hours = parseInt((duration/(1000*60*60))%24);
+    
+        hours = (hours < 10) ? "0" + hours : hours;
+        minutes = (minutes < 10) ? "0" + minutes : minutes;
+        seconds = (seconds < 10) ? "0" + seconds : seconds;
+    
+        return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+      }
+    var start = new Date();
+    var maxScrollReached = 0;
     window.onscroll = function(){
         //alert("scroll "+this.scrollY)
-        window.postMessage(JSON.stringify('sdfghjk'))
-    }
+        window.counter++;
+        var elapsed = new Date() - start;
+        if(this.scrollY > maxScrollReached){
+            maxScrollReached
+        }
+        maxScrollReached = (this.scrollY > maxScrollReached) ? this.scrollY : maxScrollReached;
+        window.postMessage(JSON.stringify('['+window.counter+'] message_from_webview : scroll_detect x='+this.scrollX+' y='+this.scrollY+' time='+msToTime(elapsed)))
+        window.postMessage(JSON.stringify('['+window.counter+'] message_from_webview : maxScrollReached='+maxScrollReached))
+    } 
     patchedPostMessage.toString = function() {
         return String(Object.hasOwnProperty).replace('hasOwnProperty', 'postMessage')
     }
