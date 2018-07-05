315b494bca90c222e3dd57bff4e50408883ae689
fabienmcl <fabien.michl@gmail.com>
2018-07-05 16:06:55 +0200

    Update : bugfixes

--- src/containers/SideMenuScreens/SimpleConcept.js
+++ src/containers/SideMenuScreens/SimpleConcept.js
@@ -8,11 +8,11 @@ import {
   Dimensions,
   TouchableOpacity,
   Modal,
-  Button,
   AsyncStorage,
   ActivityIndicator
 } from 'react-native';
 import Expo, { Font, AppLoading } from 'expo';
+import { Container, Header, Left, Body, Right, Button, Icon, Title, Footer, FooterTab } from 'native-base';
 import s1 from '../../images/slide1.png'
 import s2 from '../../images/slidey.png'
 import s3 from '../../images/slidez.png'
@@ -170,7 +170,7 @@ export default class SwiperConcept extends Component {
         );
     }
     return (
-      <Swiper style={styles.wrapper} loop={false} showsButtons={true}>
+      <Swiper style={styles.wrapper} loop={!this.state.isConnected? true:false} showsButtons={true} autoplay={!this.state.isConnected? true:false} >
         <View style={styles.slide1}>
             
                 <TouchableOpacity onPress={() => this._skipConcept()}>
@@ -178,7 +178,7 @@ export default class SwiperConcept extends Component {
                 </TouchableOpacity>
             
             
-            <Image style={{width:SCREEN_WIDTH}} source={s1}/> 
+           <Image style={ this.state.isConnected? {width:SCREEN_WIDTH, height:SCREEN_HEIGHT*3/5} : {width:SCREEN_WIDTH}} source={s1}/> 
             <Text style={styles.text}>{I18n.t('concept_welcome')}</Text>
             <Text style={{color:'#fff', textAlign: 'center'}}> {I18n.t('concept_welcome_explain')}</Text>
         </View>
@@ -186,7 +186,7 @@ export default class SwiperConcept extends Component {
             <TouchableOpacity onPress={() => this._skipConcept()}>
                 <Text style={{color: '#fff'}}> {!this.state.isConnected? I18n.t('concept_skip'): ""} </Text>
             </TouchableOpacity>
-            <Image style={{width: SCREEN_WIDTH }} source={s2}/> 
+            <Image style={ this.state.isConnected? {width:SCREEN_WIDTH, height:SCREEN_HEIGHT*3/5} : {width:SCREEN_WIDTH}} source={s2}/> 
             <Text style={styles.text}>{I18n.t('concept_discover')}</Text>
             <Text style={{color:'#fff', textAlign: 'center'}}> {I18n.t('concept_discover_explain')} </Text>
         </View>
@@ -194,7 +194,7 @@ export default class SwiperConcept extends Component {
             <TouchableOpacity onPress={() => this._skipConcept()}>
                 <Text style={{color: '#fff'}}> {!this.state.isConnected? I18n.t('concept_skip'): ""} </Text>
             </TouchableOpacity>
-            <Image style={{width: SCREEN_WIDTH }} source={s3}/> 
+            <Image style={ this.state.isConnected? {width:SCREEN_WIDTH, height:SCREEN_HEIGHT*3/5} : {width:SCREEN_WIDTH}} source={s3}/> 
             <Text style={styles.text}>{I18n.t('concept_sn')} </Text>
             <TouchableOpacity onPress={() => this._toggleModal()}>
                 <Text style={{color:'#fff', textAlign: 'center'}}> {I18n.t('concept_sn_explain')} </Text>
@@ -203,8 +203,23 @@ export default class SwiperConcept extends Component {
                 animationType="slide"
                 transparent={false}
                 visible={this.state.isModalVisible}
+                style={{backgroundColor:'#212121'}}
                 onRequestClose={() => {alert('Modal has been closed.');}}>
-                <Text>
+                <Container >
+        <Header style={{backgroundColor:'#212121',}}>
+          <Left>
+            
+          </Left>
+          <Body>
+            <Title style={{ color: 'white'}}>RENEWAL</Title>
+          </Body>
+          <Right>
+            <Button onPress={() =>this._toggleModal()} transparent>
+              <Text style={{ color: 'white'}} >Cancel</Text>
+            </Button>
+          </Right>
+        </Header>
+        <Text>
                     Comment sont utilisées mes données ?
 
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras auctor dictum auctor. 
@@ -217,12 +232,14 @@ export default class SwiperConcept extends Component {
                     Proin eget est quis sem ultrices placerat. Suspendisse potenti. Praesent mollis vitae magna eget luctus. 
                     Aliquam consectetur sem nec velit tincidunt efficitur. Praesent leo sem, fringilla at nibh vel, lobortis consectetur velit. 
                 </Text>
-                <Button
-                    onPress={() =>this._toggleModal()}
-                    title="Close"
-                    color="#007ac1"
-                    accessibilityLabel="Learn more about this purple button"
-                />
+                <Footer style={{backgroundColor:'#212121'}}>
+          <FooterTab>
+          <Button full danger  onPress={() =>this._toggleModal()} ><Text style={{ color: 'white'}}> Close </Text></Button>
+          </FooterTab>
+          </Footer>
+                
+      </Container>
+                
             </Modal>
         </View>
         
